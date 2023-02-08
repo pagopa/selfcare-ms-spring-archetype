3fa8e18ab268b89bce800dadc3575e157214ebc2
bgalgamu <kevin.si1396@gmail.com>
2023-02-08 10:31:59 +0100

    update commons and swaggerTest

--- src/main/resources/archetype-resources/app/src/test/java/web/config/SwaggerConfigTest.java
+++ src/main/resources/archetype-resources/app/src/test/java/web/config/SwaggerConfigTest.java
@@ -14,6 +14,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -22,6 +23,8 @@ import springfox.documentation.oas.annotations.EnableOpenApi;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -48,18 +51,26 @@ class SwaggerConfigTest {
     @Test
     void swaggerSpringPlugin() throws Exception {
         MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
-        mockMvc.perform(MockMvcRequestBuilders.get("/v3/api-docs").accept(MediaType.APPLICATION_JSON))
-                .andDo((result) -> {
-                    assertNotNull(result);
-                    assertNotNull(result.getResponse());
-                    final String content = result.getResponse().getContentAsString();
-                    assertFalse(content.isBlank());
-                    assertFalse(content.contains("${"), "Generated swagger contains placeholders");
-                    Object swagger = objectMapper.readValue(content, Object.class);
-                    String formatted = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(swagger);
-                    Path basePath = Paths.get("src/main/resources/swagger/");
-                    Files.createDirectories(basePath);
-                    Files.write(basePath.resolve("api-docs.json"), formatted.getBytes());
-                });
+        mockMvc.perform(MockMvcRequestBuilders.get("/v3/api-docs").accept(MediaType.APPLICATION_JSON)).andExpect(MockMvcResultMatchers.status().is2xxSuccessful()).andDo((result) -> {
+            assertNotNull(result);
+            assertNotNull(result.getResponse());
+            final String content = result.getResponse().getContentAsString();
+            checkPlaceholders(content);
+            assertFalse(content.isBlank());
+            assertFalse(content.contains("${"), "Generated swagger contains placeholders");
+            Object swagger = objectMapper.readValue(result.getResponse().getContentAsString(), Object.class);
+            String formatted = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(swagger);
+            Path basePath = Paths.get("src/main/resources/swagger/");
+            Files.createDirectories(basePath);
+            Files.write(basePath.resolve("api-docs.json"), formatted.getBytes());
+        });
+    }
+
+    private static void checkPlaceholders(String content) {
+        Pattern pattern = Pattern.compile("\\$\\{(.*?)\\}");
+        Matcher matcher = pattern.matcher(content);
+        while (matcher.find()) {
+            log.error("\033[31m An error occurred with placeholder: {}", matcher.group(1));
+        }
     }
 }
